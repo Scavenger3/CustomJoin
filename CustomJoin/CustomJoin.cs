084d8510b9757b5a5deb13090b6d35ba92a841fd
Scavenger3 <scavenger-3@hotmail.com>
2013-01-14 20:21:17 +1000

    v1.0 First Commit

---
+++ CustomJoin/CustomJoin.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using Hooks;
+using Terraria;
+using TShockAPI;
+
+namespace CustomJoin
+{
+	[APIVersion(1, 12)]
+	public class CustomJoin : TerrariaPlugin
+	{
+		public override string Name { get { return "CustomJoin"; } }
+		public override string Author { get { return "Scavenger"; } }
+		public override Version Version { get { return Assembly.GetExecutingAssembly().GetName().Version; } }
+
+		public static cjConfig cfg { get; set; }
+		int LastJoinIndex { get; set; }
+
+		public CustomJoin(Main game)
+			: base(game)
+		{
+			Order = -10;
+			cfg = new cjConfig();
+			LastJoinIndex = -1;
+		}
+
+		public override void Initialize()
+		{
+			GameHooks.Initialize += onInitialize;
+			NetHooks.GetData += onGetData;
+			NetHooks.SendData += onSendData;
+			ServerHooks.Leave += onLeave;
+		}
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing)
+			{
+				GameHooks.Initialize -= onInitialize;
+				NetHooks.GetData -= onGetData;
+				NetHooks.SendData -= onSendData;
+				ServerHooks.Leave -= onLeave;
+			}
+			base.Dispose(disposing);
+		}
+
+		void onInitialize()
+		{
+			Commands.ChatCommands.Add(new Command("customjoin.reload", cjConfig.ReloadConfig, "cjreload"));
+			Commands.ChatCommands.Add(new Command("customjoin.setjoin", CMDsetjoin, "setjoin"));
+			Commands.ChatCommands.Add(new Command("customjoin.setleave", CMDsetleave, "setleave"));
+			cjConfig.LoadConfig();
+		}
+
+		void CMDsetjoin(CommandArgs args)
+		{
+			if (args.Parameters.Count < 1)
+			{
+				args.Player.SendWarningMessage("Usage: /setjoin <text / hide / reset>");
+				return;
+			}
+
+			string message = string.Join(" ", args.Parameters);
+
+			if (!cfg.CustomMessages.ContainsKey(args.Player.Name))
+				cfg.CustomMessages.Add(args.Player.Name, new cfgMessage());
+
+			switch (message.ToLower())
+			{
+				case "hide":
+					cfg.CustomMessages[args.Player.Name].HideJoin = true;
+					cfg.CustomMessages[args.Player.Name].JoinMessage = string.Empty;
+					args.Player.SendMessage("Your join message will not be shown!", Color.MediumSeaGreen);
+					break;
+				case "reset":
+					if (cfg.CustomMessages[args.Player.Name].HideLeave == false && cfg.CustomMessages[args.Player.Name].LeaveMessage == string.Empty)
+						cfg.CustomMessages.Remove(args.Player.Name);
+					else
+					{
+						cfg.CustomMessages[args.Player.Name].HideJoin = false;
+						cfg.CustomMessages[args.Player.Name].JoinMessage = string.Empty;
+					}
+					args.Player.SendMessage("Using default TShock join message!", Color.MediumSeaGreen);
+					break;
+				default:
+					cfg.CustomMessages[args.Player.Name].HideJoin = false;
+					cfg.CustomMessages[args.Player.Name].JoinMessage = message;
+					args.Player.SendMessage("Your join message is now:", Color.MediumSeaGreen);
+					args.Player.SendInfoMessage(string.Format(cfg.CustomMessages[args.Player.Name].JoinMessage, args.Player.Name, args.Player.Country ?? string.Empty));
+					break;
+			}
+
+			cfg.Write();
+		}
+		void CMDsetleave(CommandArgs args)
+		{
+			if (args.Parameters.Count < 1)
+			{
+				args.Player.SendWarningMessage("Usage: /setleave <message / hide / reset>");
+				return;
+			}
+
+			string message = string.Join(" ", args.Parameters);
+
+			if (!cfg.CustomMessages.ContainsKey(args.Player.Name))
+				cfg.CustomMessages.Add(args.Player.Name, new cfgMessage());
+
+			switch (message.ToLower())
+			{
+				case "hide":
+					cfg.CustomMessages[args.Player.Name].HideLeave = true;
+					cfg.CustomMessages[args.Player.Name].LeaveMessage = string.Empty;
+					args.Player.SendMessage("Your leave message will not be shown!", Color.MediumSeaGreen);
+					break;
+				case "reset":
+					if (cfg.CustomMessages[args.Player.Name].HideJoin == false && cfg.CustomMessages[args.Player.Name].JoinMessage == string.Empty)
+						cfg.CustomMessages.Remove(args.Player.Name);
+					else
+					{
+						cfg.CustomMessages[args.Player.Name].HideLeave = false;
+						cfg.CustomMessages[args.Player.Name].LeaveMessage = string.Empty;
+					}
+					args.Player.SendMessage("Using default TShock leave message!", Color.MediumSeaGreen);
+					break;
+				default:
+					cfg.CustomMessages[args.Player.Name].HideLeave = false;
+					cfg.CustomMessages[args.Player.Name].LeaveMessage = message;
+					args.Player.SendMessage("Your join message is now:", Color.MediumSeaGreen);
+					args.Player.SendInfoMessage(string.Format(cfg.CustomMessages[args.Player.Name].LeaveMessage, args.Player.Name, args.Player.Country ?? string.Empty));
+					break;
+			}
+
+			cfg.Write();
+		}
+
+		void onSendData(SendDataEventArgs e)
+		{
+			try
+			{
+				if (e.MsgID == PacketTypes.ChatText && e.Handled == false && LastJoinIndex > -1 && LastJoinIndex < TShock.Players.Length && e.text.EndsWith(" has joined.") && e.number2 == Color.Yellow.R && e.number3 == Color.Yellow.G && e.number4 == Color.Yellow.B)
+				{
+					var tsplr = TShock.Players[LastJoinIndex];
+					LastJoinIndex = -1;
+
+					if (tsplr == null) return;
+
+					if (cfg.CustomMessages.ContainsKey(tsplr.Name))
+					{
+						if (cfg.CustomMessages[tsplr.Name].HideJoin)
+							e.Handled = true;
+						else if (cfg.CustomMessages[tsplr.Name].JoinMessage != string.Empty)
+							e.text = string.Format(cfg.CustomMessages[tsplr.Name].JoinMessage, tsplr.Name, tsplr.Country ?? string.Empty);
+					}
+				}
+			}
+			catch { }
+		}
+
+		void onGetData(GetDataEventArgs e)
+		{
+			try
+			{
+				if (e.MsgID == PacketTypes.TileGetSection && e.Handled == false)
+				{
+					LastJoinIndex = e.Msg.whoAmI;
+				}
+			}
+			catch { }
+		}
+
+		void onLeave(int ply)
+		{
+			try
+			{
+				var tsplr = TShock.Players[ply];
+				if (tsplr != null && cfg.CustomMessages.ContainsKey(tsplr.Name) && tsplr.ReceivedInfo)
+				{
+					if (cfg.CustomMessages[tsplr.Name].HideLeave == false && cfg.CustomMessages[tsplr.Name].LeaveMessage == string.Empty) return;
+
+					tsplr.SilentKickInProgress = true;
+					tsplr.State = 1;//Remove in tshock 4.1
+
+					if (cfg.CustomMessages[tsplr.Name].LeaveMessage != string.Empty)
+						TShock.Utils.Broadcast(string.Format(cfg.CustomMessages[tsplr.Name].LeaveMessage, tsplr.Name, tsplr.Country ?? string.Empty), Color.Yellow);
+				}
+			}
+			catch { }
+		}
+	}
+}
---
+++ CustomJoin/cjConfig.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using Newtonsoft.Json;
+using TShockAPI;
+
+namespace CustomJoin
+{
+	public class cjConfig
+	{
+		public Dictionary<string, cfgMessage> CustomMessages;
+
+		public static string cfgPath { get { return Path.Combine(TShock.SavePath, "PluginConfigs", "CustomJoinConfg.json"); } }
+		public static cjConfig Read()
+		{
+			if (!File.Exists(cfgPath))
+				return new cjConfig();
+			using (var fs = new FileStream(cfgPath, FileMode.Open, FileAccess.Read, FileShare.Read))
+			{
+				return Read(fs);
+			}
+		}
+		public static cjConfig Read(Stream stream)
+		{
+			using (var sr = new StreamReader(stream))
+			{
+				var cf = JsonConvert.DeserializeObject<cjConfig>(sr.ReadToEnd());
+				if (ConfigRead != null)
+					ConfigRead(cf);
+				return cf;
+			}
+		}
+		public void Write()
+		{
+			using (var fs = new FileStream(cfgPath, FileMode.Create, FileAccess.Write, FileShare.Write))
+			{
+				Write(fs);
+			}
+		}
+		public void Write(Stream stream)
+		{
+			var str = JsonConvert.SerializeObject(this, Formatting.Indented);
+			using (var sw = new StreamWriter(stream))
+			{
+				sw.Write(str);
+			}
+		}
+
+		public static Action<cjConfig> ConfigRead;
+
+		/*Load, Reload & Create Config*/
+		public static void ReloadConfig(CommandArgs args)
+		{
+			if (LoadConfig())
+				args.Player.SendMessage("CustomJoin config reloaded successfully!", Color.MediumSeaGreen);
+			else
+				args.Player.SendMessage("An exception occoured while reloading CustomJoin, Check the logs for more details.", Color.OrangeRed);
+		}
+		public static bool LoadConfig()
+		{
+			try
+			{
+				if (!File.Exists(cfgPath))
+					CreateConfig();
+
+				CustomJoin.cfg = cjConfig.Read();
+
+				if (CustomJoin.cfg.CustomMessages == null)
+					CustomJoin.cfg.CustomMessages = new Dictionary<string, cfgMessage>()
+					{
+						{ "SomePlayer", new cfgMessage() },
+					};
+
+				CustomJoin.cfg.Write();
+				return true;
+			}
+			catch (Exception ex)
+			{
+				Log.ConsoleError("An exception occoured while loading CustomJoin config file, Check the logs for more details.");
+				Log.Error(ex.ToString());
+				return false;
+			}
+		}
+		public static void CreateConfig()
+		{
+			File.WriteAllText(cfgPath,
+				"{" + Environment.NewLine +
+				"  \"CustomMessages\": {" + Environment.NewLine +
+				"    \"DarkOS\": {" + Environment.NewLine +
+				"      \"HideJoin\": false," + Environment.NewLine +
+				"      \"JoinMessage\": \"\"," + Environment.NewLine +
+				"      \"HideLeave\": false," + Environment.NewLine +
+				"      \"LeaveMessage\": \"DarkOS in the house... holla!\"" + Environment.NewLine +
+				"    }" + Environment.NewLine +
+				"  }" + Environment.NewLine +
+				"}");
+		}
+	}
+
+	public class cfgMessage
+	{
+		public bool HideJoin { get; set; }
+		public string JoinMessage { get; set; }
+		public bool HideLeave { get; set; }
+		public string LeaveMessage { get; set; }
+		public cfgMessage()
+		{
+			this.HideJoin = false;
+			this.JoinMessage = string.Empty;
+			this.HideLeave = false;
+			this.LeaveMessage = string.Empty;
+		}
+	}
+}
